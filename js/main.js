331b8b7c4c4233e4f3eed59b1a817ecf01ccc907
Lungelo7 <smartdawn7@gmail.com>
2021-10-06 14:41:36 +0200

    commit 6

--- js/main.js
+++ js/main.js
@@ -88,30 +88,6 @@
         }
     });
     
-    // form Thanks 
-    var form = document.getElementById("tanzarella-live");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
-}
-    
-    
     
     // Portfolio isotope and filter
     var portfolioIsotope = $('.portfolio-container').isotope({
@@ -127,6 +103,4 @@
     });
     
 })(jQuery);
-
-
 
